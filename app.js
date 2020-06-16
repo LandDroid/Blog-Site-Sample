4983380ba60fcb0bc736debd8ae0c9e8f887b914
Landon Lyle <landonlyle@live.ca>
2020-06-16 00:07:10 -0400

    Fix Heroku

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Our routes
 const routes = require("./routes.js");
-app.use("/", routes);
+app.use("/routes", routes);
 
 // Start our server
 app.listen(process.env.PORT || 3000, (port) =>
